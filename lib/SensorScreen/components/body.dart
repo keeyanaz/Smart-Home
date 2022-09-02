637ee9acd5e8f1eb9f1affad0f8c6cea90cfb981
Keeyanaz <kia0311naz@gmail.com>
2022-09-02 23:43:59 +0430

    reading data from firebase added, alert dialog addded.

--- lib/SensorScreen/components/body.dart
+++ lib/SensorScreen/components/body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:smart_home/SensorScreen/components/custom_card.dart';
 import '../../constants.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class SensorScreenBody extends StatefulWidget {
   @override
@@ -14,239 +17,280 @@ class _SensorScreenBodyState extends State<SensorScreenBody> {
     return SafeArea(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: size.width * 0.05),
-        child: Column(
-          children: [
-            SizedBox(height: size.height * 0.02),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(
-                  Icons.menu,
-                  size: 30,
-                  color: kDarkGreyColor,
-                ),
-                Text(
-                  'Home',
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                Container(
-                  height: size.height * 0.045,
-                  width: size.width * 0.095,
-                  decoration: BoxDecoration(
-                    color: kBgColor,
-                    borderRadius: BorderRadius.circular(30),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 8,
-                        offset: Offset(3, 3),
-                      ),
-                    ],
-                  ),
-                  child: Icon(
-                    Icons.notifications_none,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              SizedBox(height: size.height * 0.02),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Icon(
+                    Icons.menu,
+                    size: 30,
                     color: kDarkGreyColor,
                   ),
-                )
-              ],
-            ),
-            SizedBox(height: size.height * 0.03),
-            Row(
-              children: [
-                Container(
-                  height: 120,
-                  width: 120,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Image.asset(
-                      "assets/images/profile_picture.jpg",
-                      fit: BoxFit.cover,
+                  Text(
+                    'Home',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
                     ),
                   ),
-                ),
-                SizedBox(width: size.width * 0.05),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'JUNE 14, 2020',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w600,
+                  Container(
+                    height: size.height * 0.045,
+                    width: size.width * 0.095,
+                    decoration: BoxDecoration(
+                      color: kBgColor,
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 8,
+                          offset: Offset(3, 3),
+                        ),
+                      ],
+                    ),
+                    child: Icon(
+                      Icons.notifications_none,
+                      color: kDarkGreyColor,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: size.height * 0.03),
+              Row(
+                children: [
+                  Container(
+                    height: 120,
+                    width: 120,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        "assets/images/profile_picture.jpg",
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    Text(
-                      'Good Morning,\nMichael',
-                      style: TextStyle(
-                        color: Colors.black87,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: size.height * 0.05),
-            Row(
-              children: [
-                Expanded(
-                  child: Column(
+                  ),
+                  SizedBox(width: size.width * 0.05),
+                  Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        '40°',
+                        'JUNE 14, 2020',
                         style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
                           color: Colors.grey,
+                          fontWeight: FontWeight.w600,
                         ),
                       ),
                       Text(
-                        'TEMPERATURE',
-                        style: TextStyle(fontSize: 16, color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        '59%',
+                        'Good Morning,\nMichael',
                         style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.grey,
+                          color: Colors.black87,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
                         ),
                       ),
-                      Text(
-                        'HUMIDITY',
-                        style: TextStyle(fontSize: 16, color: Colors.grey),
-                      ),
                     ],
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: size.height * 0.05),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                CustomCard(
-                  size: size,
-                  icon: Icon(
-                    Icons.home_outlined,
-                    size: 55,
-                    color: Colors.grey.shade400,
-                  ),
-                  title: "ENTRY",
-                  statusOn: "OPEN",
-                  statusOff: "LOCKED",
-                ),
-                CustomCard(
-                  size: size,
-                  icon: Icon(
-                    Icons.lightbulb_outline,
-                    size: 55,
-                    color: Colors.grey.shade400,
-                  ),
-                  title: "LIGHTS",
-                  statusOn: "ON",
-                  statusOff: "OFF",
-                ),
-              ],
-            ),
-            SizedBox(height: size.height * 0.025),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                CustomCard(
-                  size: size,
-                  icon: Icon(
-                    Icons.opacity,
-                    size: 55,
-                    color: Colors.grey.shade400,
-                  ),
-                  title: "LEAKS",
-                  statusOn: "DETECTED",
-                  statusOff: "NOT DETECTED",
-                ),
-                CustomCard(
-                  size: size,
-                  icon: Icon(
-                    Icons.thermostat_outlined,
-                    size: 55,
-                    color: Colors.grey.shade400,
-                  ),
-                  title: "THERMOSTAT",
-                  statusOn: "ON",
-                  statusOff: "OFF",
-                ),
-              ],
-            ),
-            SizedBox(height: size.height * 0.025),
-            Container(
-              height: 75,
-              width: size.width * 0.8,
-              decoration: BoxDecoration(
-                color: kBgColor,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black12,
-                    blurRadius: 8,
-                    offset: Offset(3, 3),
-                  ),
-                  BoxShadow(
-                    color: Colors.white,
-                    blurRadius: 0,
-                    offset: Offset(-3, -3),
                   ),
                 ],
               ),
-              child: Padding(
-                padding: EdgeInsets.all(15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
+              SizedBox(height: size.height * 0.05),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
-                          "ADD",
+                          '40°',
                           style: TextStyle(
                             fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black54,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey,
                           ),
                         ),
                         Text(
-                          "NEW CONTROL",
-                          style: TextStyle(
-                            color: Colors.black45,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        )
+                          'TEMPERATURE',
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
                       ],
                     ),
-                    Icon(
-                      Icons.add,
-                      size: 40,
-                      color: Colors.black54,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '59%',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Text(
+                          'HUMIDITY',
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: size.height * 0.05),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CustomCard(
+                    size: size,
+                    icon: Icon(
+                      Icons.home_outlined,
+                      size: 55,
+                      color: Colors.grey.shade400,
+                    ),
+                    title: "ENTRY",
+                    statusOn: "OPEN",
+                    statusOff: "LOCKED",
+                  ),
+                  CustomCard(
+                    size: size,
+                    icon: Icon(
+                      Icons.lightbulb_outline,
+                      size: 55,
+                      color: Colors.grey.shade400,
+                    ),
+                    title: "LIGHTS",
+                    statusOn: "ON",
+                    statusOff: "OFF",
+                  ),
+                ],
+              ),
+              SizedBox(height: size.height * 0.025),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  CustomCard(
+                    size: size,
+                    icon: Icon(
+                      Icons.opacity,
+                      size: 55,
+                      color: Colors.grey.shade400,
+                    ),
+                    title: "LEAKS",
+                    statusOn: "DETECTED",
+                    statusOff: "NOT DETECTED",
+                  ),
+                  CustomCard(
+                    size: size,
+                    icon: Icon(
+                      Icons.thermostat_outlined,
+                      size: 55,
+                      color: Colors.grey.shade400,
+                    ),
+                    title: "THERMOSTAT",
+                    statusOn: "ON",
+                    statusOff: "OFF",
+                  ),
+                ],
+              ),
+              SizedBox(height: size.height * 0.025),
+              Container(
+                height: 75,
+                width: size.width * 0.8,
+                decoration: BoxDecoration(
+                  color: kBgColor,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 8,
+                      offset: Offset(3, 3),
+                    ),
+                    BoxShadow(
+                      color: Colors.white,
+                      blurRadius: 0,
+                      offset: Offset(-3, -3),
                     ),
                   ],
                 ),
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "ADD",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black54,
+                            ),
+                          ),
+                          Text(
+                            "NEW CONTROL",
+                            style: TextStyle(
+                              color: Colors.black45,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                      Icon(
+                        Icons.add,
+                        size: 40,
+                        color: Colors.black54,
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Future loadData() async {
+    String jsonString = await getJsonFromFirebaseRestAPI();
+    final jsonResponse = json.decode(jsonString);
+    setState(() {
+      for (Map<String, dynamic> i in jsonResponse) {
+        if (i.toString() == '{month: Jan, sales: 35}' || i.toString() == '{month: Apr, sales: 32}') {
+          showDialog<String>(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: const Text('Motion detection status:'),
+              content: const Text('The motion has detected!'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'OK'),
+                  child: const Text('OK'),
+                ),
+              ],
+            ),
+          );
+          return;
+        }
+      }
+
+    });
+  }
+
+  @override
+  void initState() {
+    loadData();
+    super.initState();
+  }
+}
+
+Future<String> getJsonFromFirebaseRestAPI() async {
+  String url = 'https://flutterdemo-f6d47.firebaseio.com/chartSalesData.json';
+  http.Response response = await http.get(Uri.parse(url));
+  return response.body;
 }
